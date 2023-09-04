5070e931f4b60cfb95c3d8f9bae187531d8d6d02
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-09-04 16:47:55 +0200

    feat: get message from server from .env file

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get(API_URL)
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('Une erreur s\'est produite lors de la récupération des données du serveur : ', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="message">Message from the server : <b>{message}</b> <br></br><i style={{fontSize:"20px"}}>from <b>{API_URL}</b></i></div>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const allowlist = ["http://localhost:3000", "http://localhost:3001"];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
+app.get("/", cors(corsOptionsDelegate), (req, res) => {
+  const message = process.env.MESSAGE || "";
+  res.json({ message: message });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
