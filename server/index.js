b79dd637e4311517563807882bd3dddf09d4afa8
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-09-05 10:29:02 +0200

    feat: connect server to mongodb service

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const port = 3001;
+const mongoose = require("mongoose");
+
+const MONGO_DB_URL = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongodb:27017/${process.env.MONGO_INITDB_DATABASE}`;
+
+mongoose
+  .connect(MONGO_DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
 const allowlist = ["http://localhost:3000", "http://localhost:3001"];
 const corsOptionsDelegate = function (req, callback) {
