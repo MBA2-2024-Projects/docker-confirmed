28c5e7c79f0670a3da0d372ab52bdbe01695367b
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-09-05 10:41:13 +0200

    feat: client to server to database

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,13 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   const [message, setMessage] = useState('');
+  const [value, setValue] = useState('');
 
   useEffect(() => {
     axios.get(API_URL)
       .then(response => {
         setMessage(response.data.message);
+        setValue(response.data.value);
       })
       .catch(error => {
         console.error('Une erreur s\'est produite lors de la récupération des données du serveur : ', error);
@@ -23,6 +25,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div className="message">Message from the server : <b>{message}</b> <br></br><i style={{fontSize:"20px"}}>from <b>{API_URL}</b></i></div>
+        <div className="message">Number of database connections : <b>{value}</b></div>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
+const Counter = require("./counter");
 
 const MONGO_DB_URL = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongodb:27017/${process.env.MONGO_INITDB_DATABASE}`;
 
@@ -15,6 +16,38 @@ mongoose
     console.error("MongoDB connection error:", error);
   });
 
+async function incrementCounterByName(counterName) {
+  try {
+    let counter = await Counter.findOne({ name: counterName });
+    if (!counter) {
+      counter = new Counter({ name: counterName, value: 1 });
+    } else {
+      counter.value += 1;
+    }
+    await counter.save();
+    console.log(`Compteur "${counterName}" incrémenté à ${counter.value}`);
+  } catch (error) {
+    console.error("Erreur lors de l'incrémentation du compteur :", error);
+  }
+}
+
+async function getCounterValueByName(counterName) {
+  try {
+    const counter = await Counter.findOne({ name: counterName });
+    if (counter) {
+      return counter.value;
+    } else {
+      return -1;
+    }
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération de la valeur du compteur :",
+      error
+    );
+    throw error;
+  }
+}
+
 const allowlist = ["http://localhost:3000", "http://localhost:3001"];
 const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
@@ -26,9 +59,11 @@ const corsOptionsDelegate = function (req, callback) {
   callback(null, corsOptions);
 };
 
-app.get("/", cors(corsOptionsDelegate), (req, res) => {
+app.get("/", cors(corsOptionsDelegate), async (req, res) => {
+  incrementCounterByName("counter");
   const message = process.env.MESSAGE || "";
-  res.json({ message: message });
+  const value = await getCounterValueByName("counter");
+  res.json({ message: message, value: value });
 });
 
 app.listen(port, () => {
